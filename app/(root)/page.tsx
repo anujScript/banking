77c4a6cc690d2d0a3c537856c720e18fc0d6b7ae
anujScript <anuj@auranft.co>
2024-08-09 04:06:54 +0530

    feat: left side bar and right side car added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceaBox from "@/components/TotalBalanceaBox";
+import RightSideBar from "@/components/RightSideBar";
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Anuj", lastName: "Jain", email: "a@a.com" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "User"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceaBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={99.1}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 109.1 }, { currentBalance: 1000.5 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
